03180e1db7119e311b93a69535f5baca544cfc67
Lizzie Turner <hello@lizzieturner.com>
2018-08-30 22:36:25 +0000

    Fix exception

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -11,13 +11,13 @@ def top_ten(subreddit):
         returns the top ten posts for a given subreddit
     '''
     user = {'User-Agent': 'Lizzie'}
-    url = requests.get('https://www.reddit.com/r/{}/hot/.json?count=10'
+    url = requests.get('https://www.reddit.com/r/{}/hot/.json?limit=10'
                        .format(subreddit), headers=user).json()
     try:
         for post in url.get('data').get('children'):
             print(post.get('data').get('title'))
     except Exception:
-        return None
+        print(None)
 
 
 if __name__ == "__main__":
