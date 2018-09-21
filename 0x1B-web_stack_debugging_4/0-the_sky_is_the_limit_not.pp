4871bf0d8cbb590d9fef0d3f8644ac528e512c86
Lizzie Turner <hello@lizzieturner.com>
2018-09-21 05:52:28 +0000

    Add nginx restart

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,6 @@
 # fixes broken requests
-exec { 'increase file limit':
-  command => 'sed -i "s/15/15000/g" /etc/default/nginx',
+exec { 'increase file limit' :
+  command => 'sed -i "s/15/15000/g" /etc/default/nginx; service nginx restart',
   path    => '/usr/sbin:/usr/bin:/sbin:/bin'.
   onlyif  => 'test -e /etc/default/nginx',
 }
