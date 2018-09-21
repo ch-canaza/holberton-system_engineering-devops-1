5cf908c90acee6a1ec7f1c0b1839bac4ae626c2a
Lizzie Turner <hello@lizzieturner.com>
2018-09-21 05:46:40 +0000

    Create 1-user_limit

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,6 @@
+# fix login issue
+exec { 'increase file limits':
+  command => ' sudo sed -i "s/4/6000/g" /etc/security/limits.conf',
+  path    => '/usr/bin:/usr/sbin:/bin:/sbin',
+  onlyif  => 'test -e /etc/security/limits.conf',
+}
