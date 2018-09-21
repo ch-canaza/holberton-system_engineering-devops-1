85b1e0776c9ec461cd6f3189e8f3a118d58b8163
Lizzie Turner <hello@lizzieturner.com>
2018-09-21 05:54:30 +0000

    Add usr/local/bin to path

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,6 @@
 # fixes broken requests
 exec { 'increase file limit' :
   command => 'sed -i "s/15/15000/g" /etc/default/nginx; service nginx restart',
-  path    => '/usr/sbin:/usr/bin:/sbin:/bin'.
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'.
   onlyif  => 'test -e /etc/default/nginx',
 }
