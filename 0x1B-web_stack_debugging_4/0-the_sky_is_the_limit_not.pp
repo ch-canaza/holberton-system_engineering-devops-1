1a2b9b5c2ac21a5d9251ebb0b86d5397bd30bbff
Lizzie Turner <hello@lizzieturner.com>
2018-09-21 05:14:51 +0000

    Create 0-the_sky_is_the_limit_not

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,6 @@
+# fixes broken requests
+exec { 'increase file limit':
+  command => 'sed -i "s/15/50000/g" /etc/default/nginx',
+  path    => '/usr/sbin:/usr/bin:/sbin:/bin'.
+  onlyif  => 'test -e /etc/default/nginx',
+}
